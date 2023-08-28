1dd3fc3ab723047fab33ea0781b1636f5bfd7c61
Abdul Azeez Kemp <aakemp786@gmail.com>
2023-08-29 00:37:22 +0200

    update

---
+++ random.js
@@ -0,0 +1,70 @@
+const randomList = [
+    {
+        word:"paris",
+        hint:"What is the capital of France?"
+    },
+    {
+        word:"mars",
+        hint:`Which planet is known as the "Red Planet"?`
+    },
+    {
+        word:"bark",
+        hint: "What's the hard outer layer of a tree trunk?"
+    },
+    {
+        word:"arachnophobia",
+        hint:`What is the scientific term for the "fear of spiders"?`
+    },
+    {
+        word:"au",
+        hint:"What is the chemical symbol for gold?"
+    },
+    {
+        word:"evaporation",
+        hint:"What's the process of turning liquid into vapor?"
+    },
+    {
+        word:"mercury",
+        hint:"What's the planet closest to the sun?"
+    },
+    {
+        word:"lion",
+        hint:`Which animal is known as the "King of the Jungle"?`
+    },
+    {
+        word:"skin",
+        hint:"What is the largest organ in the human body?"
+    },
+    {
+        word:"photosynthesis",
+        hint:"What is the process by which plants make their own food using sunlight?"
+    },
+    {
+        word:"amnesia",
+        hint:"What is the medical condition characterized by the sudden, severe loss of memory, often caused by brain trauma or injury?"
+    },
+    {
+        word:"jupiter",
+        hint:"What is the largest planet in our solar system?"
+    },
+    {
+        word:"echo",
+        hint:" I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?"
+    },
+    {
+        word:"keyboard",
+        hint:"I have keys but open no locks. I have space but no room. You can enter, but you can't go outside. What am I?"
+    },
+    {
+        word:"footsteps",
+        hint:"The more you take, the more you leave behind. What am I?"
+    },
+    {
+        word:"map",
+        hint:" I have cities but no houses, forests but no trees, and rivers but no water."
+    },
+    {
+        word:"breath",
+        hint:"I'm light as a feather, yet the strongest person can't hold me for much longer than a minute. What am I?"
+    },
+]
